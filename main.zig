3a0409f3ada8045bf013bdc0f95ec80011e7e43a
RyEggGit <ryegg360@gmail.com>
2024-12-27 19:40:01 -0500

    fix some shit idk zig be hard

--- main.zig
+++ main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const CodeSize = u16;
+const CodeSize = u12;
 pub fn main() !void {
     const path = "./compress_me.txt";
     const fd = try std.posix.open(path, .{ .ACCMODE = .RDONLY }, 0);
@@ -11,11 +11,18 @@ pub fn main() !void {
     std.debug.print("{s} is {} bytes!\n", .{ path, stat.size });
     std.debug.print("Lets compress this shit\n", .{});
 
-    const file = std.fs.File{ .handle = fd };
-    var buffered_reader = std.io.bufferedReader(file.reader());
-    var file_iterator = FileIterator.init(buffered_reader.reader());
+    // Read the file into buffer
+    const buffer = try std.heap.page_allocator.alloc(u8, @intCast(stat.size));
+    defer std.heap.page_allocator.free(buffer);
 
-    var compressed_data = try compress(&file_iterator);
+    // Read the file contents into the buffer
+    const bytes_read = try std.posix.read(fd, buffer);
+    if (bytes_read != stat.size) {
+        return error.IncompleteRead;
+    }
+
+    const iter = std.mem.window(u8, buffer, @intCast(stat.size), @sizeOf(u8));
+    var compressed_data = try compress(iter);
     defer compressed_data.deinit();
 }
 
@@ -30,28 +37,24 @@ fn compress(iter: std.mem.WindowIterator(u8)) !std.ArrayList(CodeSize) {
     while (iter.next()) |byte| {
         var next_seq = try curr_seq.clone();
         try next_seq.append(byte);
-
+        // If the next sequence is in the dictionary, append the byte and continue
         if (dict.map.get(next_seq.items)) {
-            // Sequence exists in dictionary, continue building it
             try curr_seq.append(byte);
-        } else {
-            // Sequence not found, output code for current sequence
-            if (dict.map.get(curr_seq.items)) |code| {
-                try compressed.append(code);
-            }
-
-            // Add new sequence to dictionary
-            if (dict.counter >= 65535) {
-                std.debug.panic("Dictionary is full! (Just expand code size and should be fine :)) \n", .{});
-            }
-
-            dict.counter += 1;
-            try dict.map.put(next_seq.items, dict.counter);
-
-            // Reset current sequence to just the current byte
-            curr_seq.clearAndFree();
-            try curr_seq.append(byte);
+            continue;
         }
+
+        // Sequence not found, output code for current sequence
+        if (dict.map.get(curr_seq.items)) |code| {
+            try compressed.append(code);
+        }
+
+        // Add new sequence to dictionary
+        dict.counter += 1;
+        try dict.map.put(next_seq.items, dict.counter);
+
+        // Reset current sequence to just the current byte
+        curr_seq.clearAndFree();
+        try curr_seq.append(byte);
     }
 
     // If we still have a sequence in progress, output its code
@@ -68,46 +71,27 @@ fn compress(iter: std.mem.WindowIterator(u8)) !std.ArrayList(CodeSize) {
     return compressed;
 }
 
-const FileIterator = struct {
-    reader: anytype,
-    buffer: [1]u8 = undefined,
-
-    const Self = @This();
-
-    pub fn init(reader: anytype) Self {
-        return Self{
-            .reader = reader,
-        };
-    }
-
-    pub fn next(self: *Self) !?u8 {
-        const amt = self.reader.read(&self.buffer) catch |err| switch (err) {
-            error.EndOfStream => return null,
-            else => return err,
-        };
-        if (amt == 0) return null;
-        return self.buffer[0];
-    }
-};
-
 const Dictionary = struct {
     map: std.AutoHashMap([]u8, CodeSize),
-    counter: CodeSize,
+    counter: CodeSize = 0,
 
     const Self = @This();
 
-    pub fn init(self: *Self) void {
-        self.map = std.AutoHashMap([]u8, u16).init(std.heap.page_allocator);
-        self.counter = 0;
+    pub fn init(allocator: *std.mem.Allocator) !Self {
+        var map = try std.AutoHashMap([]u8, CodeSize).init(allocator);
+        const counter: CodeSize = 0;
 
         // Fill the initial dictionary with all the UTF-8 characters
-        var i: u16 = 0;
-        while (i < 256) : (i += 1) {
-            var char_seq = std.ArrayList(u8).init(std.heap.page_allocator);
-            defer char_seq.deinit();
-            char_seq.append(u8(i));
-            self.map.put(char_seq.toOwnedSlice(), i);
+        for (0..256) |i| {
+            const key = &[_]u8{u8(i)};
+            try map.put(key, i);
         }
+
+        return Self{ .map = map, .counter = counter };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.map.deinit();
     }
 
     pub fn add(self: *Self, curr_seq: std.ArrayList(u8), next_char: u8) CodeSize {
